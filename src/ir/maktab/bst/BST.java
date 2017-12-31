cdf9675cdbe4085af015198b78934016c8385dff
babak <babako64@gmail.com>
2017-12-31 20:46:38 +0330

    commit4

--- src/ir/maktab/bst/BST.java
+++ src/ir/maktab/bst/BST.java
@@ -1,5 +1,125 @@
 package ir.maktab.bst;
 
-public class BST {
+import ir.maktab.collection.Collection;
 
+public class BST<E extends Comparable<E>> implements Collection<E>,Comparable<E> {
+	Bnode<E> root=null ;
+	private int size=0;
+	
+	@Override
+	public int size() {
+		
+		return size;
+	}
+
+	@Override
+	public void clear() {
+		
+		root=null;
+	}
+
+	@Override
+	public boolean isEmplty() {
+		
+		return size==0?true:false;
+	}
+
+	@Override
+	public boolean contains(E var) {
+		
+		Bnode<E> temp=root;
+		
+		if(temp!=null) {
+		while(temp!=null) {
+			
+			if(temp.getData()==var) {
+				return true;
+			}else if(temp.getData().compareTo(var)>0){
+				temp=temp.getLeft();
+			}else {
+				temp=temp.getRight();
+			}
+				
+			}
+		}
+		
+		return false;
+	}
+	public void add(E var) {
+	
+		Bnode<E> temp;
+		Bnode<E> node = new Bnode<>();
+		node.setData(var);
+		
+		if(root==null) {
+			root=node;
+			root.setLeft(null);
+			root.setRight(null);
+			System.out.println("root");
+		}else {
+			temp=root;
+			while(true) {
+				
+				if(node.getData().compareTo(temp.getData())<0) {
+					
+					if(temp.getLeft()==null) {
+						temp.setLeft(node);
+						System.out.println("L add");
+						size++;
+						return;
+					}
+					temp = temp.getLeft();
+				}else {
+					
+					if(temp.getRight()==null) {
+						temp.setRight(node);
+						System.out.println("R add");
+						size++;
+						return;
+					}
+					temp = temp.getRight();
+				}
+				
+			}
+			
+		}
+	}
+	public E getMinElement() {
+		
+		Bnode<E> temp=root;
+		
+		if(temp!=null) {
+		while(temp.getLeft()!=null) {
+			
+			temp=temp.getLeft();
+				
+			}
+		}
+		
+		return temp.getData();
+	}
+	public E getMaxElement() {
+		
+		Bnode<E> temp=root;
+		
+		if(temp!=null) {
+		while(temp.getRight()!=null) {
+			
+			temp=temp.getRight();
+				
+			}
+		}
+		
+		return temp.getData();
+	}
+
+	@Override
+	public int compareTo(E arg0) {
+		
+		int result= this.compareTo(arg0);
+		if(result==0) {
+			return 0;
+		}else 
+		return result<0?-1:1;
+	}
 }
--- src/ir/maktab/deque/Deque.java
+++ src/ir/maktab/deque/Deque.java
@@ -1,5 +1,137 @@
 package ir.maktab.deque;
 
-public class Deque {
+import ir.maktab.collection.Collection;
+import ir.maktab.exceptions.UnderflowException;
+import ir.maktab.stack.Node;
 
+public class Deque<E> implements Collection<E> {
+	private Qnode<E> first=null;
+	private Qnode<E> last=null;
+	private int size=0;
+	
+	public void pushBack(E var) {
+		
+		Qnode<E> node = new Qnode<>();
+		if(last!=null) {
+		node.setData(var);
+		node.setNext(last);
+		node.setBack(null);
+		last=node;
+		size++;
+		}else {
+			node.setData(var);
+			node.setNext(last);
+			node.setBack(null);
+			last=node;
+			first=node;
+			size++;
+		}
+	}
+	public void pushFront(E var) {
+		Qnode<E> node = new Qnode<>();
+		if(first!=null) {
+		node.setData(var);
+		node.setNext(null);
+		node.setBack(first);
+		first.setNext(node);
+		first=node;
+		size++;
+		}else {
+			node.setData(var);
+			node.setNext(null);
+			node.setBack(first);
+			first=node;
+			last=node;
+			size++;
+		}
+	}
+	public E front() {
+		return first.getData();
+	}
+	public E back() {
+		return last.getData();
+	}
+	public E popFront() {	
+		Qnode<E> temp;
+		if(!isEmplty()) {
+		if(first.getBack()!=null) {
+		temp = first;
+		first=first.getBack();
+		first.setNext(null);
+		size--;
+		}else {
+			temp = first;
+			first=null;
+			size--;
+		}
+		}else {
+			throw new UnderflowException();
+		}
+		return temp.getData();
+	}
+	public E popBack() {
+		Qnode<E> temp;
+		if(!isEmplty()) {
+		if(last.getNext()!=null) {
+		temp=last;
+		last=last.getNext();
+		last.setBack(null);
+		size--;
+		}else {
+			temp=last;
+			last=null;
+			size--;
+		}
+		}else {
+			throw new UnderflowException();
+		}
+		return temp.getData();
+	}
+	public E getElement(int i) {
+		Qnode<E> temp ;
+		if(i<size) {
+		temp=first;
+		for(int J=2;J<=size;J++) {
+			
+			temp=temp.getNext();
+		}
+		}else {
+			throw new IndexOutOfBoundsException();
+		}
+		return temp.getData();
+	}
+
+	@Override
+	public int size() {
+		
+		return size;
+	}
+
+	@Override
+	public void clear() {
+		
+		first=null;
+		last=null;
+		size=0;
+	}
+
+	@Override
+	public boolean isEmplty() {
+		
+		return size==0?true:false;
+	}
+
+	@Override
+	public boolean contains(E var) {
+		
+		Qnode<E> temp ;
+		temp=first;
+		for(int i=2;i<=size;i++) {
+			if(temp.getData()==var) {
+				return true;
+			}
+			temp=temp.getNext();
+		}
+		return false;
+	}
 }
